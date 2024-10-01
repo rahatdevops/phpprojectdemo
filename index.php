377f1297d4716d640b3753287c3be6f07d852c48
Saeed Mahud Rahat <118961502+rahatdevops@users.noreply.github.com>
2024-10-01 13:21:21 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello Devops world</p>'; ?>
+        <?php echo '<p>Hello Devops world first cicd</p>'; ?>
     </body>
 </html>
